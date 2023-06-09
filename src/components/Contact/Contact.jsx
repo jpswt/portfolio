9c848c4f52ac5bcd9e04dd58a8fdab70fc244b50
jpswt <joel_swt@yahoo.com>
2023-06-08 23:38:35 -0500

    fix contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import wave from './wave.svg';
 import './Contact.css';
 
 const Contact = () => {
+	const [statusMsg, setStatusMsg] = useState('');
 	const form = useRef();
 
 	const sendEmail = (e) => {
@@ -19,6 +20,11 @@ const Contact = () => {
 			.then(
 				(result) => {
 					console.log(result.text);
+					setStatusMsg('Your request has been sent!');
+					form.current.reset();
+					setTimeout(() => {
+						setStatusMsg('');
+					}, 2000);
 				},
 				(error) => {
 					console.log(error.text);
@@ -30,21 +36,37 @@ const Contact = () => {
 		<div className="contact-container" id="contact">
 			<h1 className="contact-title">Contact Me</h1>
 			<div className="form-wrapper">
-				<img src={wave} alt="wave border image" />
 				<form ref={form} onSubmit={sendEmail} className="form-container">
-					<label className="form-labels" htmlFor="email">
-						Email
-					</label>
-					<input type="text" id="email" name="user_email" className="inputs" />
-					<label className="form-labels" htmlFor="name">
-						Name
-					</label>
-					<input id="name" type="text" name="user_name" className="inputs" />
-					<label className="form-labels" htmlFor="message">
-						Message
-					</label>
-					<textarea className="inputs" id="message" name="message" rows="6" />
-					<input type="submit" value="SEND" />
+					<input
+						type="text"
+						id="email"
+						name="user_email"
+						className="inputs"
+						placeholder="Email"
+						required
+					/>
+
+					<input
+						id="name"
+						type="text"
+						name="user_name"
+						className="inputs"
+						placeholder="Name"
+						required
+					/>
+
+					<textarea
+						className="inputs"
+						id="message"
+						name="message"
+						rows="6"
+						style={{ resize: 'none' }}
+						placeholder={`Let's connect...`}
+						required
+					/>
+					<button className="btn" type="submit">
+						{statusMsg ? statusMsg : 'Send'}
+					</button>
 				</form>
 			</div>
 		</div>
